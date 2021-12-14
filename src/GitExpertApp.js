32e6ee25e5ff532a24859a3d765b51aef43cade7
lauralopezfre <lfregoso@sahuarolabs.com>
2021-12-14 14:08:37 -0700

    Se obtiene un nuevo renderizado cuando se agrega un nuevo componente

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GitExpertApp = () => {
+
+    // const categorias = ['Mario', 'Luigi', 'Sorbito'];
+
+    const [personajes, setPersonajes] = useState(['Mario', 'Luigi', 'Sorbito'])
+    
+    // Renderizar cuando se añada un nuevo personaje
+    const handleAdd = () => {
+        // Sabe que el estado cambio y hace una renderización
+        setPersonajes([...personajes, 'Princesa'])
+    }
+
     return (
         <div>
             <h2>GitExpertApp</h2>
             <hr/>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    personajes.map(categoria =>{
+                        return <li key={categoria}>{categoria}</li>
+                    })
+                }
+            </ol>
+            
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GitExpertApp } from './GitExpertApp';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <GitExpertApp />,
   document.getElementById('root')
 );
 
-
-//GitExpertApp
